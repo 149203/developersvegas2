b81dd94ea5d28a5e979fe109f82ab9a65ae9f04e
149203 <149203@gmail.com>
2019-01-26 15:06:36 -0800

    created a developers/upsert API

--- routes/api/developers.js
+++ routes/api/developers.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const developer = require('../../models/developer')
+const _pick = require('lodash/pick')
 
 // @route      GET api/developers/test
 // @desc       Tests the developers route
@@ -9,5 +11,48 @@ router.get('/test', (req, res) =>
       msg: 'The developers route works.',
    })
 )
+
+// @route      api/developers/upsert
+// @desc       Upserts a developer
+// @access     Public
+router.post('/upsert', (req, res) => {
+   const { first_name, last_name, email, id } = req.body
+   const new_developer = new developer({
+      first_name,
+      last_name,
+      email,
+      id,
+   }) // create a new developer with the info from the req body
+   console.log(new_developer)
+
+   developer
+      .findOneAndUpdate(
+         {
+            email: req.body.email,
+         },
+         select_properties(new_developer), // UGLY: There's got to be a better way to upsert everything but the _id
+         { upsert: true, new: true, setDefaultsOnInsert: true },
+         () => {
+            console.log('UPSERTED')
+         }
+      )
+      .then(developer => {
+         res.json(developer)
+      })
+      .catch(err => console.log(err))
+})
+
+function select_properties(document) {
+   return _pick(document, [
+      'id',
+      'first_name',
+      'last_name',
+      'email',
+      'portfolio_url',
+      'photo_url_sm',
+      'photo_url_md',
+      'photo_url_orig',
+   ])
+}
 
 module.exports = router
