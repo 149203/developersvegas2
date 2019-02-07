991f45efc79bebb2fd292102935d2dc235eaefc5
149203 <149203@gmail.com>
2019-02-07 11:07:43 -0800

    Outlined how to get a list of all technologies & popularities

---
+++ scripts/technologies.js
@@ -0,0 +1,56 @@
+// Put all technologies into their own gist - allow for open source pull requests
+// Put all technologies and popularity into their own gist, describe method for calculating popularity - allow for open source pull requests
+
+// List of sites to scrape data from
+// For wikipedia articles, take internal wiki-links only. Remove parentheticals. Remove any unlinked explanatory text. Remove stuff after ' – '.
+
+// Popularity += 20
+// If in the third section (50-100) on https://www.tiobe.com/tiobe-index/
+
+// Popularity += 40
+// If in the second table on https://www.tiobe.com/tiobe-index/
+
+// Popularity += 50
+// If in the first table on https://www.tiobe.com/tiobe-index/
+
+// Popularity += 75
+// Every chart and tab under: https://insights.stackoverflow.com/survey/2018/#most-loved-dreaded-and-wanted
+
+// Popularity += 200
+// Every chart under "Most Popular Technologies": https://insights.stackoverflow.com/survey/2018/#most-popular-technologies
+
+// List of All Programming Languages and Technologies — Only make this once, then open source it and allow pull requests.
+// Alphabetize and visually prune it
+
+// Bulleted items
+// Bulleted items on: https://en.wikipedia.org/wiki/General-purpose_programming_language
+// Every bullet item from Array Languages up to System Languages. Wiki-linked only: https://en.wikipedia.org/wiki/List_of_programming_languages_by_type
+// All bulleted items in this section: https://en.wikipedia.org/wiki/Fourth-generation_programming_language#Examples
+// All bulleted items in this section: https://en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)#Line_continuation
+// All bulleted items here, except "High-level programming language": https://en.wikipedia.org/wiki/Category:High-level_programming_languages#Pages_in_category
+// All bulleted items, expect the first four: https://en.wikipedia.org/wiki/Category:Web_frameworks#Pages_in_category
+// All bulleted items: https://en.wikipedia.org/wiki/Category:JavaScript_web_frameworks
+// All bulleted items: https://en.wikipedia.org/wiki/Category:PHP_frameworks#Pages_in_category
+// All bulleted items except the first one: https://en.wikipedia.org/wiki/Category:Python_web_frameworks#Pages_in_category
+// All bulleted items: https://en.wikipedia.org/wiki/Category:Rich_Internet_application_frameworks
+// Bulleted items in this section: https://en.wikipedia.org/wiki/Open-source_computing_hardware#Partially_open-source_hardware
+// Bulleted items in this section: https://en.wikipedia.org/wiki/Open-source_computing_hardware#Fully_open-source_hardware
+// Bulleted items in this section: https://en.wikipedia.org/wiki/List_of_open-source_hardware_projects#Electronics
+
+// Table columns
+// first column. https://en.wikipedia.org/wiki/Comparison_of_programming_languages#General_comparison
+// First col of https://en.wikipedia.org/wiki/Comparison_of_programming_languages_(object-oriented_programming)#Object_construction_and_destruction
+// First and 4th cols: https://en.wikipedia.org/wiki/System_programming_language#Major_languages
+// First col here: https://en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)#Statements
+// Second col of each table: https://en.wikipedia.org/wiki/Timeline_of_programming_languages
+// First col of each table: https://en.wikipedia.org/wiki/Comparison_of_web_frameworks
+// First col: https://en.wikipedia.org/wiki/List_of_rich_Internet_application_frameworks
+// First col of every table here: https://en.wikipedia.org/wiki/List_of_content_management_systems#Free_and_open_source_software
+// Second col of this table: https://en.wikipedia.org/wiki/Comparison_of_single-board_computers#General_comparison
+// 3rd and 4th cols. https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites
+
+// Other
+// The wiki-links that follow each dt: https://en.wikipedia.org/wiki/Solution_stack#Examples (not the Stack names)
+// Every chart under: https://insights.stackoverflow.com/survey/2018/#most-popular-technologies
+// Every chart and tab under: https://insights.stackoverflow.com/survey/2018/#most-loved-dreaded-and-wanted
+// All languages here, parsed for colons, commas, and parentheticals: https://www.tiobe.com/tiobe-index/programming-languages-definition/#instances
