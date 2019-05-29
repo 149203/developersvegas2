0b6dc2e69ed16f047463a41af5d13064b2b1fac2
149203 <149203@gmail.com>
2019-05-28 19:42:12 -0700

    cleaned up post_members.js script

--- scripts/post_members.js
+++ scripts/post_members.js
@@ -1,8 +1,6 @@
 const members = require('../data/members')
 const axios = require('axios')
 const fs = require('fs')
-const _head = require('lodash/head')
-const _take_right = require('lodash/takeRight')
 const _trim = require('lodash/trim')
 const _for_each = require('lodash/forEach')
 
@@ -32,41 +30,3 @@ _for_each(members, member => {
          })
    }
 })
-
-// post_to_members_collection(members)
-
-function post_to_members_collection(members) {
-   const member = _head(members)
-   // the data has some null objects in it
-   if (member) {
-      const new_member = {
-         first_name: _trim(member.first_name),
-         last_name: _trim(member.last_name),
-         email: _trim(member.email),
-         row_id: member._id,
-         portfolio_url: _trim(member.portfolio),
-      }
-      axios
-         .post(`${server_url}/api/v1/members`, new_member)
-         .then(res => {
-            console.log(res.data)
-            restart(members)
-         })
-         .catch(err => {
-            console.log(err.response.data)
-            fs.appendFileSync(
-               './logs/post_members.txt',
-               `${JSON.stringify(new_member)} \n\n`,
-               err => console.log(err)
-            )
-            restart(members)
-         })
-   } else restart(members)
-}
-
-function restart(members) {
-   const remaining_members = _take_right(members, members.length - 1)
-   console.log(`${remaining_members.length} members remaining`)
-   if (remaining_members.length > 0)
-      post_to_members_collection(remaining_members)
-}
