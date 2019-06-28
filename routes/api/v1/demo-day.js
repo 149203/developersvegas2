fe72ae002b6f1537b1add3a05fdcd6a3336565ac
149203 <149203@gmail.com>
2019-06-28 13:40:02 -0700

    upsert inserts new doc

--- routes/api/v1/demo-day.js
+++ routes/api/v1/demo-day.js
@@ -5,11 +5,9 @@ const member_model = require('../../../models/member')
 const presentation_model = require('../../../models/presentation')
 const technology_model = require('../../../models/technology')
 const xref_presentation_technology_model = require('../../../models/xref_presentation_technology')
-const slug_format = require('../../../utils/slug_format')
-const append_slug_suffix = require('../../../utils/append_slug_suffix')
-const create_row_id = require('../../../utils/create_row_id')
 const convert_undefined = require('../../../utils/convert_undefined')
 const get_object_id = require('../../../utils/get_object_id')
+const upsert = require('../../../utils/upsert')
 const validate_input_for_presentation = require('../../../validation/presentation')
 const cast_to_object_id = require('mongodb').ObjectID
 const _for_each = require('lodash/forEach')
@@ -25,21 +23,20 @@ router.post('/', (req, res) => {
    //    return res.status(400).json(errors)
    // }
 
-   const agreement_id = cast_to_object_id('5d0e6f4d63f3b43f2830cd4f') // hard-coded ID of agreement 0.1
+   // hard-coded stuff for now
+   const agreement_id = cast_to_object_id('5d0e6f4d63f3b43f2830cd4f')
    const has_accepted_agreement = true
 
    _for_each(demo_days, async demo_day => {
-      const event_id = get_object_id(event_model, {
+      const event_id = await get_object_id(event_model, {
          started_on: new Date(demo_day.event.date),
       })
 
-      const member_id = get_object_id(member_model, {
+      const member_id = await get_object_id(member_model, {
          row_id: demo_day.member.row_id,
       })
 
       const presentation_obj = {}
-      const presentation_technology_obj = {}
-      // These are fields that can be updated via the API
       presentation_obj.agreement_id = agreement_id
       presentation_obj.has_accepted_agreement = has_accepted_agreement
       presentation_obj.event_id = event_id
@@ -60,94 +57,22 @@ router.post('/', (req, res) => {
          demo_day.presentation.video_iframe
       ) // optional
 
-      const technology_ids = demo_day.technologies.map(technology =>
-         get_object_id(technology_model, { row_id: technology.id })
-      ) // an array of object_ids
-
-      await presentation_model
-         .findOne({
+      upsert({
+         payload: presentation_obj,
+         collection: presentation_model,
+         options: {
+            should_create_slug: true,
+            should_create_row_id: true,
+         },
+         filter: {
             event_id: presentation_obj.event_id,
             order: presentation_obj.order,
-         })
-         .then(async presentation => {
-            if (presentation) {
-               // if our search matches a document, update
-               await presentation_model
-                  .findByIdAndUpdate(
-                     presentation._id,
-                     { $set: presentation_obj },
-                     { new: true }
-                  )
-                  .then(updated_presentation => {
-                     // console.log({ updated_presentation })
-                     presentation_technology_obj.presentation_id =
-                        updated_presentation._id
-                     // append to custom JSON response
-                  })
-                  .catch(err => res.status(400).json(err))
-            } else {
-               // Create presentation
-               let slug = slug_format(
-                  presentation_obj.title || 'untitled-project'
-               ) // 'title-of-presentation' or 'untitled-project'
-               presentation_obj.slug = await append_slug_suffix(
-                  presentation_model,
-                  slug
-               )
-               presentation_obj.row_id = await create_row_id(presentation_model)
-
-               await new presentation_model(presentation_obj)
-                  .save()
-                  .then(presentation => {
-                     // console.log({ presentation })
-                     presentation_technology_obj.presentation_id =
-                        presentation._id
-                     // update presentation_technology
-                     // append to custom JSON response
-                  })
-                  .catch(err => res.status(400).json(err))
-            }
-         })
-         .catch(err => res.status(400).json(err))
-
-      await technology_ids.forEach(async row_id => {
-         await console.log({ row_id })
-         await technology_model
-            .findOne({ row_id })
-            .then(async technology => {
-               presentation_technology_obj.technology_id = technology._id
-
-               console.log({ presentation_technology_obj })
-
-               await xref_presentation_technology_model
-                  .findOne({
-                     presentation_id:
-                        presentation_technology_obj.presentation_id,
-                     technology_id: presentation_technology_obj.technology_id,
-                  })
-                  .then(async xref => {
-                     // only if this xref doesn't exist, create it
-                     if (!xref) {
-                        console.log(
-                           `I didnt find this xref: ${JSON.stringify(
-                              presentation_technology_obj
-                           )}`
-                        )
-                        await new xref_presentation_technology_model(
-                           presentation_technology_obj
-                        )
-                           .save()
-                           .then(presentation_technology => {
-                              // console.log({ presentation_technology })
-                              // append to custom JSON response
-                           })
-                           .catch(err => res.status(400).json(err))
-                     } // else console.log({ xref })
-                  })
-                  .catch(err => res.status(400).json(err))
-            })
-            .catch(err => res.status(400).json(err))
+         },
       })
+
+      // const technology_ids = demo_day.technologies.map(technology =>
+      //    get_object_id(technology_model, { row_id: technology.id })
+      // ) // an array of object_ids
    })
    // res.json(presentation) return a custom response with all fields
 })
--- utils/upsert.js
+++ utils/upsert.js
@@ -1,32 +1,89 @@
-const _has = require('lodash/has')
+const has = require('lodash/has')
+const is_empty = require('lodash/isEmpty')
 const mongoose = require('mongoose')
+const slug_format = require('./slug_format')
 const append_slug_suffix = require('./append_slug_suffix')
 const create_row_id = require('./create_row_id')
 
-module.exports = upsert = async (upsert_obj, collection, filter_params_obj) => {
-   let response
-   let errors
+module.exports = upsert = async query => {
+   const response = []
+   const errors = []
 
-   // if (options_obj.should_create_slug) {
-   //    // send a formatted slug in upsert_obj.slug
-   //    upsert_obj.slug = await append_slug_suffix(collection, upsert_obj.slug)
-   // }
+   if (typeof query.filter === undefined || is_empty(query.filter)) {
+      insert_new_doc(query)
+      // return {response, errors} ?
+   } else if (has(query.filter, '_id')) {
+      update_doc_by_id(query, response, errors, insert_new_doc)
+      // return {response, errors} ?
+   } else {
+      update_doc_by_params(query, response, errors, insert_new_doc)
+      // return {response, errors} ?
+   }
+   console.log({ response, errors })
+}
 
-   // if (options_obj.should_create_row_id) {
-   //    upsert_obj.row_id = await create_row_id(collection)
-   // }
+async function insert_new_doc(query, response, errors) {
+   console.log('made it to insert')
+   console.log(query.options)
+   if (
+      query.options.should_create_slug &&
+      (!has(query.payload, 'slug') || query.payload.slug === '')
+   ) {
+      console.log('we should create a slug')
+      const slug = slug_format(query.payload.title || 'untitled-project')
+      query.payload.slug = await append_slug_suffix(query.collection, slug)
+   }
 
-   await collection
-      .findOneAndUpdate(
-         filter_params_obj,
-         { $set: upsert_obj },
-         {
-            new: true,
-            upsert: true,
-         }
-      )
-      .then(doc => (response = doc))
-      .catch(err => (errors = { findOneAndUpdate: err }))
+   if (query.options.should_create_row_id) {
+      console.log('we should create a row_id')
+      query.payload.row_id = await create_row_id(query.collection)
+   }
 
-   return { response, errors }
+   await console.log(query.payload)
+
+   await new query.collection(query.payload)
+      .save()
+      .then(doc => {
+         console.log({ doc })
+         response.push(doc)
+      })
+      .catch(err => {
+         console.log({ err })
+         errors.push({ save: err })
+      })
+}
+
+async function update_doc_by_id(query, response, errors) {
+   await query.collection.findByIdAndUpdate(
+      query.filter._id,
+      query.payload,
+      {
+         new: true,
+         runValidators: true,
+      },
+      (err, doc) => {
+         if (err) {
+            // we didn't find the doc
+            insert_new_doc(query, response, errors)
+         } else response.push(doc)
+      }
+   )
+}
+
+async function update_doc_by_params(query, response, errors) {
+   await query.collection
+      .findOneAndUpdate(query.filter, query.payload, {
+         new: true,
+         runValidators: true,
+      })
+      .then(doc => {
+         if (!doc) {
+            console.log('didnt find the doc')
+            insert_new_doc(query, response, errors)
+         } else response.push(doc)
+      })
+      .catch(err => {
+         console.log({ findOneAndUpdate: err })
+         errors.push({ findOneAndUpdate: err })
+      })
 }
