0327de9d8ef8adadd59d9a5d2991430f37131e5d
149203 <149203@gmail.com>
2019-07-13 10:16:51 -0700

    parsing the demo_day_presentations object

--- routes/api/v1/demo-day.js
+++ routes/api/v1/demo-day.js
@@ -18,7 +18,7 @@ const to_lower = require('lodash/toLower')
 // @desc       Create all data for a demo day
 // @access     Public
 router.post('/', async (req, res) => {
-   const demo_days = req.body
+   const demo_days = JSON.parse(req.body.demo_day_presentations)
 
    // hard-coded stuff for now
    const agreement_id = cast_to_object_id('5d0e6f4d63f3b43f2830cd4f')
