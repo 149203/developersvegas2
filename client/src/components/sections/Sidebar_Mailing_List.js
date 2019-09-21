20aea8266df5abbbb98f2fbfea15751ef6fb1d0f
149203 <149203@gmail.com>
2019-09-21 13:48:45 -0700

    Futzing with the modal. Learned things. Made a note.

--- client/src/components/sections/Sidebar_Mailing_List.js
+++ client/src/components/sections/Sidebar_Mailing_List.js
@@ -49,9 +49,6 @@ class Sidebar_Mailing_List extends Component {
             this.setState({
                errors: {},
                is_success_modal_open: true,
-               first_name: '',
-               last_name: '',
-               email: '',
             })
          })
          .catch(err => this.setState({ errors: err.response.data }))
@@ -59,8 +56,15 @@ class Sidebar_Mailing_List extends Component {
 
    render() {
       const { errors } = this.state
-      const close_success_modal = () =>
-         this.setState({ is_success_modal_open: false })
+      const close_success_modal = () => {
+         this.setState({
+            is_success_modal_open: false,
+            first_name: '',
+            last_name: '',
+            email: '',
+         })
+      }
+
       return (
          <Sidebar>
             <h4>Stay in the loop</h4>
@@ -130,17 +134,31 @@ class Sidebar_Mailing_List extends Component {
             </form>
             <div className="clearfix"></div>
 
+            {/*
+               MAH DUDE
+               If you want to break the modal into its own component, you'll have to use Redux (or a parent component, but you don't want that).
+               Because the open and closed state of the modal exists in this component AND the modal component, so you must share its state.
+            */}
+
             <Modal
                show={this.state.is_success_modal_open}
                onHide={close_success_modal}
             >
                <Modal.Header closeButton>
-                  <Modal.Title>Modal heading</Modal.Title>
+                  <Modal.Title>We'll keep you in the loop!</Modal.Title>
                </Modal.Header>
                <Modal.Body>
-                  Woohoo, you're reading this text in a modal!
+                  <p>
+                     Thanks for signing up, {this.state.first_name}. We'll keep
+                     you in the loop.
+                  </p>
+                  <button
+                     className="btn btn-primary float-right"
+                     onClick={close_success_modal}
+                  >
+                     AWESOME
+                  </button>
                </Modal.Body>
-               <Modal.Footer></Modal.Footer>
             </Modal>
          </Sidebar>
       )
---
+++ client/src/components/uis/Modal_Confirmation.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react'
+import Modal from 'react-bootstrap/Modal'
+
+export default Modal_Confirmation
