ab317cbe99634e51d3d4bf7b08c9ec8492c3bb5b
149203 <149203@gmail.com>
2019-10-05 14:46:53 -0700

    console.log doc

--- utils/get_object_id.js
+++ utils/get_object_id.js
@@ -2,7 +2,10 @@ module.exports = get_object_id = async (collection, find_by_obj) => {
    let object_id
    await collection
       .findOne(find_by_obj)
-      .then(doc => (object_id = doc._id))
+      .then(doc => {
+         console.log({ doc })
+         object_id = doc._id
+      })
       .catch(err => (object_id = err))
    return object_id
 }
