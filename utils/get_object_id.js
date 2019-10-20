c4c8dc6de0d083375178e937ff40d26dc9a95858
149203 <149203@gmail.com>
2019-10-20 09:49:08 -0700

    debugging no member_id in demo-day API

--- utils/get_object_id.js
+++ utils/get_object_id.js
@@ -1,9 +1,13 @@
+const has = require('lodash/has')
+
 module.exports = get_object_id = async (collection, find_by_obj) => {
    let object_id
    await collection
       .findOne(find_by_obj)
       .then(doc => {
-         console.log('GOT MEMBER ID:', doc)
+         if (has(doc, 'first_name')) {
+            console.log('GOT MEMBER ID:', doc)
+         }
          object_id = doc._id
       })
       .catch(err => (object_id = err))
