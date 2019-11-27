a18f317b06c508a17a8c57cbe10faaf017780f13
149203 <149203@gmail.com>
2019-11-27 08:28:04 -0800

    get_vimeo_videos

--- scripts/get_vimeo_videos.js
+++ scripts/get_vimeo_videos.js
@@ -1,20 +1,11 @@
-// update all video information for all presentations with videos in the database
+// gets all video information from Vimeo
 
 // Imports
 require('dotenv').config({ path: '../.env' })
-const mongoose = require('mongoose')
 const Vimeo = require('vimeo').Vimeo
 const fs = require('fs')
 const get_chars_between = require('../utils/get_chars_between')
 const convert_friendly_date_to_num_str = require('../utils/convert_friendly_date_to_num_str')
-const get_object_id = require('../utils/get_object_id')
-
-// Databases
-const development_db_uri = process.env.development_db_uri
-const production_db_uri = process.env.production_db_uri
-
-// SELECT DATABASE
-const db = development_db_uri
 
 const vimeo_account = new Vimeo(
    process.env.vimeo_client_id,
@@ -27,6 +18,7 @@ vimeo_account.request(
    {
       method: 'GET',
       path: '/users/96172694/videos',
+      query: { per_page: 80, page: 1 }, // NOTE: YOU CAN ONLY GET A MAX OF 100 PER PAGE
    },
    (error, body) => {
       if (error) {
@@ -40,26 +32,18 @@ vimeo_account.request(
                video_screenshot_url: video.pictures.sizes[2].link,
                video_screenshot_with_play_url:
                   video.pictures.sizes[2].link_with_play_button,
-               //member_id: get_member_id(video.description),
-               member_first_name: get_first_name(video.description), // TODO: remove, get member_id
-               member_last_name: get_last_name(video.description), // TODO: remove, get member_id
-               event_id: get_event_id(video.description),
-               event_title: 'Demo Day', // TODO: remove, get event_id
-               event_started_on: get_event_started_on(video.description), // TODO: remove, get event_id
-               description: video.description, // TODO: remove
+               member_first_name: get_first_name(video.description),
+               member_last_name: get_last_name(video.description),
+               event_title: 'Demo Day',
+               event_started_on: get_event_started_on(video.description),
+               description: video.description,
             }
          })
          fs.writeFileSync(
             'logs/vimeo_results.json',
             JSON.stringify(cleaned_video_data)
          )
-
-         // Find all event_ids by {event_started_on, event_title}
-         // Find all member_ids by {first_name, last_name}
-         // Create an array of objects for every permutation, adding event_id and member_id properties
-         // updateMany in presentation_model by {event_id, member_id}
-         // Set the 5 video properties
-         // console.log if it doesn't exist
+         console.log(`${cleaned_video_data.length} items returned`)
       }
    }
 )
@@ -91,22 +75,4 @@ function get_event_started_on(description) {
       /\.\sSee\sLas\sVegas/
    )
    return Number(convert_friendly_date_to_num_str(friendly_date) + '1200')
-}
-
-async function get_event_id(description) {
-   const title = 'Demo Day'
-   const started_on = get_event_started_on(description)
-   const collection = require('../models/event')
-   console.log(title, started_on)
-   let event_id
-   await mongoose
-      .connect(db)
-      .then(async () => {
-         event_id = await get_object_id(collection, { title, started_on })
-      })
-      .catch(err => {
-         console.log(err)
-      })
-   console.log(event_id)
-   return event_id
 }
