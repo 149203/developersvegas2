45a22caf3fe1a1e169b16e2d67f4293f3a073c60
149203 <149203@gmail.com>
2019-12-08 13:26:35 -0800

    REDUX_DEVTOOLS_EXTENSION fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import thunk from 'redux-thunk'
 import rootReducer from './state/root_reducer'
 
@@ -12,8 +13,9 @@ const store = createStore(
    initial_state,
    compose(
       applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-         window.__REDUX_DEVTOOLS_EXTENSION__()
+      (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+         compose
    )
 )
 
