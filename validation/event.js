e7d29d00f3dc8ab76c13c22a46ab8e5269096d6e
149203 <149203@gmail.com>
2020-02-08 09:50:48 -0800

    removed event validation for now

--- validation/event.js
+++ validation/event.js
@@ -20,16 +20,16 @@ module.exports = function validate_input_for_event(input) {
       is_active,
    } = input
 
-   title = convert_to_str(title)
-   if (validator.isEmpty(input.title)) {
-      errors.title = 'A title for the event is required.'
-   } else if (!validator.isLength(input.title, { max: 80 })) {
-      errors.title =
-         "I can't believe this event has a title that long. It should be no more than 80 characters."
-   } else if (validator.matches(input.title, /-\d+\s*$/)) {
-      errors.title =
-         'Hey, I know this is weird. But your event title cannot end with a dash followed by a number.'
-   }
+   // title = convert_to_str(title)
+   // if (validator.isEmpty(input.title)) {
+   //    errors.title = 'A title for the event is required.'
+   // } else if (!validator.isLength(input.title, { max: 80 })) {
+   //    errors.title =
+   //       "I can't believe this event has a title that long. It should be no more than 80 characters."
+   // } else if (validator.matches(input.title, /-\d+\s*$/)) {
+   //    errors.title =
+   //       'Hey, I know this is weird. But your event title cannot end with a dash followed by a number.'
+   // }
 
    // started_on = convert_empty_to_str(started_on)
    // if (validator.isEmpty(input.started_on)) {
@@ -45,50 +45,50 @@ module.exports = function validate_input_for_event(input) {
    //    errors.ended_on = 'ended_on must be a number'
    // }
 
-   location_name = convert_to_str(location_name)
-   if (validator.isEmpty(location_name)) {
-      errors.location_name = 'location_name is required.'
-   }
+   // location_name = convert_to_str(location_name)
+   // if (validator.isEmpty(location_name)) {
+   //    errors.location_name = 'location_name is required.'
+   // }
 
-   location_street_1 = convert_to_str(location_street_1)
-   if (validator.isEmpty(location_street_1)) {
-      errors.location_street_1 = 'location_street_1 is required.'
-   }
+   // location_street_1 = convert_to_str(location_street_1)
+   // if (validator.isEmpty(location_street_1)) {
+   //    errors.location_street_1 = 'location_street_1 is required.'
+   // }
 
-   location_city = convert_to_str(location_city)
-   if (validator.isEmpty(location_city)) {
-      errors.location_city = 'location_city is required.'
-   }
+   // location_city = convert_to_str(location_city)
+   // if (validator.isEmpty(location_city)) {
+   //    errors.location_city = 'location_city is required.'
+   // }
 
-   location_state = convert_to_str(location_state)
-   if (validator.isEmpty(location_state)) {
-      errors.location_state = 'location_state is required.'
-   }
+   // location_state = convert_to_str(location_state)
+   // if (validator.isEmpty(location_state)) {
+   //    errors.location_state = 'location_state is required.'
+   // }
 
-   location_zip = convert_to_str(location_zip)
-   if (validator.isEmpty(location_zip)) {
-      errors.location_zip = 'location_zip is required.'
-   }
+   // location_zip = convert_to_str(location_zip)
+   // if (validator.isEmpty(location_zip)) {
+   //    errors.location_zip = 'location_zip is required.'
+   // }
 
-   location_url = convert_to_str(location_url)
-   if (validator.isEmpty(location_url)) {
-      errors.location_url = 'location_url is required.'
-   }
+   // location_url = convert_to_str(location_url)
+   // if (validator.isEmpty(location_url)) {
+   //    errors.location_url = 'location_url is required.'
+   // }
 
-   cost = convert_to_str(cost)
-   if (validator.isEmpty(cost)) {
-      errors.cost = 'cost is required.'
-   }
+   // cost = convert_to_str(cost)
+   // if (validator.isEmpty(cost)) {
+   //    errors.cost = 'cost is required.'
+   // }
 
-   description = convert_to_str(description)
-   if (validator.isEmpty(description)) {
-      errors.description = 'description is required.'
-   }
+   // description = convert_to_str(description)
+   // if (validator.isEmpty(description)) {
+   //    errors.description = 'description is required.'
+   // }
 
-   is_active = convert_empty_to_str(is_active)
-   if (is_active !== '' && typeof is_active !== 'boolean') {
-      errors.is_active = 'is_active must be a Boolean.'
-   }
+   // is_active = convert_empty_to_str(is_active)
+   // if (is_active !== '' && typeof is_active !== 'boolean') {
+   //    errors.is_active = 'is_active must be a Boolean.'
+   // }
 
    console.log({ errors, is_valid: is_empty(errors) })
 
