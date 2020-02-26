838ad7dabff84200512bb6969a44ecc25f6ca976
149203 <149203@gmail.com>
2020-02-26 10:49:31 -0800

    added 404 page; react-router excluding API route

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,47 @@ import './scss/master.scss' // applies global scss styles
 import Home from './components/layouts/Home'
 import Sign_In from './components/layouts/Sign_In'
 import List from './components/layouts/List'
+import Four_Oh_Four from './components/layouts/Four_Oh_Four'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux' // provides our application with a store, it has to wrap around everything
 import store from './store'
 
+// function App() {
+//    return (
+//       <Provider store={store}>
+//          <Router>
+//             <div className="App">
+//                <Route exact path="/" component={Home} />
+//                <Route exact path="/hello" component={Sign_In} />
+//                <Route exact path="/list" component={List} />
+//             </div>
+//          </Router>
+//       </Provider>
+//    )
+// }
+
 function App() {
-   return (
-      <Provider store={store}>
-         <Router>
-            <div className="App">
-               <Route exact path="/" component={Home} />
-               <Route exact path="/hello" component={Sign_In} />
-               <Route exact path="/list" component={List} />
-            </div>
-         </Router>
-      </Provider>
-   )
+   const api_regex = /^\/api\/.*/
+   if (api_regex.test(window.location.pathname)) {
+      return <div />
+   } else {
+      // if not using the /api/ routes, use react router
+      return (
+         <Provider store={store}>
+            <Router>
+               <div className="App">
+                  <Switch>
+                     <Route exact path="/" component={Home} />
+                     <Route exact path="/hello" component={Sign_In} />
+                     <Route exact path="/list" component={List} />
+                     <Route component={Four_Oh_Four} />
+                  </Switch>
+               </div>
+            </Router>
+         </Provider>
+      )
+   }
 }
 
 export default App
---
+++ client/src/components/layouts/Four_Oh_Four.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import disappointed_image from '../../img/disappointed.gif'
+
+export default function Four_Oh_Four() {
+   return (
+      <div className="container">
+         <div className="row">
+            <div className="col-12 mt-3">
+               <h1 className="font-weight-light mb-3 mb-lg-4">
+                  This page doesn't exist.
+               </h1>
+               <img
+                  src={disappointed_image}
+                  className=""
+                  alt="Gif of Kevin Sorbo in Hercules saying he is disappointed."
+               />
+               <p className="lead mt-4">
+                  Click your browser's back button or go to the{' '}
+                  <a href="/" title="https://www.developers.vegas">
+                     Las Vegas Developers homepage
+                  </a>
+                  .
+               </p>
+            </div>
+         </div>
+      </div>
+   )
+}
