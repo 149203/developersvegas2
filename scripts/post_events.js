9e56b5ed0490283520184a29f21e1b67e7ed9ca4
149203 <149203@gmail.com>
2020-05-28 10:57:53 -0700

    add july's event info

--- scripts/post_events.js
+++ scripts/post_events.js
@@ -15,18 +15,19 @@ let _id = null
 const event = {
    _id,
    title: 'Demo Day',
-   started_on: 202007111200,
-   ended_on: 202007111500,
-   cost: 'Free + free food!',
+   started_on: 202007101400,
+   ended_on: 202007101700,
+   cost: 'Free!',
    description:
-      "Demo Day is an open-mic-style event for coders to show and tell what they're working on and meet people. Sign up at noon. All ages, programming languages, and skill levels are welcome.",
-   location_name: 'PunchCode',
-   location_street_1: 'Historic 5th Street School',
-   location_street_2: '401 S 4th St',
+      "Demo Day is an open-mic-style event for coders to show and tell what they're working on and meet people. All ages, programming languages, and skill levels are welcome.",
+   location_name: 'Online!',
+   location_street_1: 'Join the Las Vegas Developers Slack Channel',
+   location_street_2: '',
    location_city: 'Las Vegas',
    location_state: 'NV',
    location_zip: '89101',
-   location_url: 'https://punchcode.org/',
+   location_url:
+      'https://join.slack.com/t/lasvegasdevelopers/shared_invite/enQtOTQyODg1NzIyNDY0LTVjMjYzNDkwMjYxMmZjMTZhM2UwZjY5ZTZlODE3NDNjZWM3Yjg4MTk0YjNiZDlkMzkyNTRlOWI3OGJjY2U1MTc',
 }
 
 console.log(convert_datetime_num_to_date(event.started_on))
@@ -37,8 +38,8 @@ function post_event(event) {
    console.log(event)
    axios
       .post(`${server_url}/api/v1/events`, event)
-      .then(res => {
+      .then((res) => {
          console.log('Back from the API:', res.data)
       })
-      .catch(err => console.log('ERROR:', err.response))
+      .catch((err) => console.log('ERROR:', err.response))
 }
